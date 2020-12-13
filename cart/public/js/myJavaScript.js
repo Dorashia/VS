d348a585164b79a2d4adfa94bc44f4f33653f053
Dorashia <nothing392112@outlook.com>
2020-12-13 22:38:53 +0800

    total

--- cart/public/js/myJavaScript.js
+++
@@ -1,9 +0,0 @@
-function updateTotalAmount() {
-    var sum = 0;
-    var element = document.getElementById("{{$cart->productID}}");
-    if(element.checked == true) {
-        sum += Number(element.value);
-    }
-
-    document.getElementById('amount').value = sum.toFixed(2);
-}
